786663e57655a8114b4a45309efcae7a111402a3
Mark Ohde <msohde1@buffs.wtamu.edu>
2020-11-17 10:11:09 -0500

    Ready for grading

--- app.py
+++ app.py
@@ -1,2 +1,136 @@
 # Chapter 3.1 Introduction, no code
 # Chapter 3.2 Conditional Statements
+# Mark Ohde
+
+print("Chapter 3.2 Conditional Statements")
+temperature = 15
+if temperature > 30:
+    print("It's warm")
+    print("Drink Water")
+elif temperature > 20:
+    print("Its nice")
+
+else:
+    print("It's cold")
+print("Done")
+print("*"*30)
+
+# Chapter 3.3 Ternary Operator
+print("Chapter 3.3 Ternary Operator")
+age = 12
+message = "Eligible" if age >= 18 else "Not eligible"
+print(message)
+
+cost = 300
+status = "expensive" if cost > 100 else "Cheap"
+print(status)
+print("*"*30)
+
+# Chapter 3.4 Logical Operators
+print("Chapter 3.4 Logical Operators")
+high_income = False
+good_credit = True
+student = False
+
+if (high_income or good_credit) and not student:
+    print("Eligible")
+else:
+    print("Not eligible")
+print("*"*30)
+
+# Chapter 3.5 Short-circuit Evaluation
+print("#Chapter 3.5 Short-circuit evaluation")
+high_income = False
+good_credit-True
+student = True
+
+if high_income or good_credit or not student:
+    print("Eligible")
+
+# Chapter 3.6 Chaining Comparison Operators
+print("#Chapter 3.6 Chaining Comparator Operators")
+# age should be between 18 and 65
+age = 22
+if 18 <= age < 65:
+    print("Eligiable")
+print("*"*30)
+
+# Chapter 3.8 For Loops
+print("#Chapter 3.8 For Loops")
+for number in range(3):
+    print("Attempts", number+1, (number+1)*".")
+
+for number in range(1, 4):
+    print("Attempts", number+1, (number+1)*".")
+
+# how to count by twos
+for number in ranger(1, 10, 2):
+    print("Attempts", number+1, (number+1)*".")
+
+# how to count down from 10 to zero
+for number in range(10, 0, -1):
+    print(number)
+print("*"*30)
+
+# Chapter 3.9 For..Else
+print("#Chapter 3.9 For..Else")
+# if failed
+successful = False
+for number in range(3):
+    print("Attempt")
+    if successful:
+        print("Successful")
+        break
+else
+print("Attempted 3 times and failed")
+print("*"*30)
+
+# if successful
+successful = True
+for number in range(3):
+    print("Attempt")
+    if successful:
+        print("Successful")
+        break
+else
+print("Attempted 3 times and failed")
+print("*"*30)
+
+# Chapter 3.10 Nested Loops
+print("#Chapter 3.10 Nested Loops")
+for x in range(5):
+    for y in range(3):
+        print(f"({X},{y})")
+
+# Chapter 3.11 Iterables
+print("#Chapter 3.11 Iterables")
+for x in [1, 2, 3, 4]:
+    print(x)
+print("*"*30)
+
+# Chapter 3.12 While Loops
+print("#Chapter 3.12 While Loops")
+command = ""
+while command.lower() != "quit":
+    command = input("Enter any command or 'quit' >")
+    print("ECHO", command)
+print("*"*30)
+
+# Chapter 3.13 Infinite Loops
+print("#Chapter 3.13 Infinite Loops")
+while True:
+    command = input("Enter any command or 'quit' >")
+    print("ECHO", command)
+    if command.lower() == "quit":
+        break
+print("*"*30)
+
+# Chapter 3.14 Exercise
+print("#Chapter 3.14 Exercise")
+counter = 0
+for number in range(1, 10):
+    if number % 2 == 0:
+        print(number)
+        counter += 1
+print(f"we have {counter} even numbers")
+print("*"*30)
